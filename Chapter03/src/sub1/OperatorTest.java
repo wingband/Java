e4731b182d47a1eed5cc79f82d6f7f67f37cb435
bigdata <bigdata@DESKTOP-ED0D4BE>
2020-04-29 11:35:08 +0900

    2020/04/29 Ch3

---
+++ Chapter03/src/sub1/OperatorTest.java
@@ -0,0 +1,26 @@
+package sub1;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+		// 산술연산자
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 4;
+		
+		int rs1 = num1 + num2;
+		int rs2 = num1 - num2;
+		int rs3 = num1 * num2;
+		int rs4 = num1 / num2;
+		int rs5 = num1 % num2;
+		
+		System.out.println("re1 : "+rs1);
+		System.out.println("re2 : "+rs2);
+		System.out.println("re3 : "+rs3);
+		System.out.println("re4 : "+rs4);
+		System.out.println("re5 : "+rs5);
+		
+		
+	}
+}
