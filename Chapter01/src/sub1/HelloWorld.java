32c762eaa11f6b46277c252f7543f01d3b30561a
bigdata <bigdata@DESKTOP-ED0D4BE>
2020-04-29 11:34:10 +0900

    2020/04/29 Chapter1

---
+++ Chapter01/src/sub1/HelloWorld.java
@@ -0,0 +1,21 @@
+package sub1;
+
+/*
+ * 
+ * 날짜 : 2020/04/29
+ * 이름 : 임진욱
+ * 내용 : 자바 HelloWorld 출력하기
+ * 
+ * 
+ */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("HelloWorld");
+		System.out.println("Hello!!");
+		
+	}
+
+}
